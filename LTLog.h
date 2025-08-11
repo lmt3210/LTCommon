630e12be7a1d30f2b6e3a8598cbc4107aaec93e9
Larry Taylor <software@larrymtaylor.com>
2025-08-11 06:57:50 -0400

    Update statusToString to show strings for audio unit errors; increase struct message length to 1024 characters.

--- LTLog.h
+++ LTLog.h
@@ -55,15 +55,70 @@ static inline void LTLog(os_log_t log, NSString *logFile,
     va_end(args);
 }
 
+struct LTauErrors
+{
+    NSString *message;
+    OSStatus code;
+};
+
+static const struct LTauErrors auErrorList[] =
+{
+    { @"kAudio_ParamError", -50},
+    // These are from AUComponent.h - Audio Unit errors enum
+    { @"kAudioUnitErr_InvalidProperty", -10879 },
+    { @"kAudioUnitErr_InvalidParameter", -10878 },
+    { @"kAudioUnitErr_InvalidElement", -10877 },
+    { @"kAudioUnitErr_NoConnection", -10876 },
+    { @"kAudioUnitErr_FailedInitialization", -10875 },
+    { @"kAudioUnitErr_TooManyFramesToProcess", -10874 },
+    { @"kAudioUnitErr_InvalidFile", -10871 },
+    { @"kAudioUnitErr_UnknownFileType", -10870 },
+    { @"kAudioUnitErr_FileNotSpecified", -10869 },
+    { @"kAudioUnitErr_FormatNotSupported", -10868 },
+    { @"kAudioUnitErr_Uninitialized", -10867 },
+    { @"kAudioUnitErr_InvalidScope", -10866 },
+    { @"kAudioUnitErr_PropertyNotWritable", -10865 },
+    { @"kAudioUnitErr_CannotDoInCurrentContext", -10863 },
+    { @"kAudioUnitErr_InvalidPropertyValue", -10851 },
+    { @"kAudioUnitErr_PropertyNotInUse", -10850 },
+    { @"kAudioUnitErr_Initialized", -10849 },
+    { @"kAudioUnitErr_InvalidOfflineRender", -10848 },
+    { @"kAudioUnitErr_Unauthorized", -10847 },
+    { @"kAudioUnitErr_MIDIOutputBufferFull", -66753 },
+    { @"kAudioComponentErr_InstanceTimedOut", -66754 },
+    { @"kAudioComponentErr_InstanceInvalidated", -66749 },
+    { @"kAudioUnitErr_RenderTimeout", -66745 },
+    { @"kAudioUnitErr_ExtensionNotFound", -66744 },
+    { @"kAudioUnitErr_InvalidParameterValue", -66743 },
+    { @"kAudioUnitErr_InvalidFilePath", -66742 },
+    { @"kAudioUnitErr_MissingKey", -66741 },
+    { @"kAudioUnitErr_ComponentManagerNotSupported", -66740 },
+    { @"kAudioUnitErr_MultipleVoiceProcessors", -66635 }
+};
+
 static inline NSString *statusToString(OSStatus code)
 {
-    char s[5] = { (char)((code >> 24) & 0xff), (char)((code >> 16) & 0xff),
-                  (char)((code >> 8) & 0xff), (char)(code & 0xff), 0 };
+    for (int i = 0; i < (sizeof(auErrorList) / sizeof(struct LTauErrors)); i++)
+    {
+        if (auErrorList[i].code == code)
+        {
+            return auErrorList[i].message;
+        }
+    }
     
-    return [NSString stringWithUTF8String:s];
+    if (code > 0)
+    {
+        char s[5] = { (char)((code >> 24) & 0xff), (char)((code >> 16) & 0xff),
+            (char)((code >> 8) & 0xff), (char)(code & 0xff), 0 };
+        return [NSString stringWithUTF8String:s];
+    }
+    else
+    {
+        return @"????";
+    }
 }
 
-#define LT_STRUCT_MESSAGE_LENGTH  256
+#define LT_STRUCT_MESSAGE_LENGTH  1024
 
 static char structStr[LT_STRUCT_MESSAGE_LENGTH];
 
